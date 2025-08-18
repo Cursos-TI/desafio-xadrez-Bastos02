70a4f9a94446c85f9e62b539d45084bf213283fa
Bastos <guilhermeaugusto0606060@gmail.com>
2025-08-18 17:52:54 +0000

    movimentação do bspo

--- xadrez.c
+++ xadrez.c
@@ -4,13 +4,21 @@ int main() {
     
 // MOVIMENTAÇAO DA TORRE
 int torre = 0;
+printf("--- MOVIMENTAÇÃO DA TORRE ---\n");
 
 for(torre; torre <5; torre++)
     printf("-> DIREITA\n");
 
+// MOVIMENTAÇÃO DO BISPO
+int bispo = 0;
+printf("--- MOVIMENTAÇÃO DO BISPO ---\n");
 
 
-
+while (bispo <5)
+{
+    printf(" ↑ CIMA-DIREITA ->\n");
+    bispo++;
+}
 
 
     return 0;
